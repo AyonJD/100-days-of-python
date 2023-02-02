b1803379940f02c7a4215172ee7bda5e42962841
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-02 23:21:52 +0600

    Operators

---
+++ Day-07-operators/main.py
@@ -0,0 +1,11 @@
+# Floor Division Operator ------> // This is a division but removes the decimal part of the result. Returns only the integer part of the result.
+
+print(8 // 3)  # 2
+
+# Modulo Operator ------> % This is a division but returns the remainder of the division. 
+
+print(5 % 3)  # 2
+
+# Exponent Operator ------> ** This is a power operator. It returns the first number to the power of the second number.
+
+print(2 ** 3)  # 8
