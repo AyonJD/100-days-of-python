c352fc18d1a2dbd05501202d95516554cd3ba4b4
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-28 20:07:54 +0600

    String method in python

--- Day-11-string/main.py
+++ Day-11-string/main.py
@@ -28,3 +28,25 @@ We can loop through the string using for loop.
 
 for char in name:
     print(char)
+
+
+# String Slicing & Operations on String------------------------>
+# Length of a String
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a", len1, "letter word.")
+
+pie = "ApplePie"
+print(pie[:5]) #Returns characters from beginning to 5th index (not included)
+print(pie[6])	#returns the 6th index
+print(pie[:5])      #Slicing from Start
+print(pie[5:])      #Slicing till End
+print(pie[2:6])     #Slicing in between
+print(pie[-8:])     #Slicing using negative index
+
+print(fruit[-1: - 3]) #It means print(fruit[len(fruit) -1:len(fruit) - 3]), returns 4th to 2nd(not included) index, here returns nothing cause 4th to 2nd index is not possible
+print(fruit[-3:-1]) #returns 2nd to 4th()not included index, here returns 'ng'
+
+'''
+So the fact is while slicing the first index is included and the last index is not included. So after : the index position will be given index - 1.
+'''
